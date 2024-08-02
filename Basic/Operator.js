4dc383949a1aed1abb2ed8d48bd2cc74c40f52d1
Utkarsh Misal <110691605+Utkarsh-876@users.noreply.github.com>
2024-08-02 12:32:17 +0000

    container config done

---
+++ Basic/Operator.js
@@ -0,0 +1,112 @@
+// Arithmetic Operator
+// --------------------
+// let a=10;
+// let b=5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+
+
+
+// Unary Operator
+//----------------
+
+// 1. Pre-Increment
+
+//let a=10;
+//console.log(++a); //11
+
+// 2. post- Increment
+//console.log(a++)//10
+
+// 3. Pre-Decrement
+//console.log(--a);  // 9
+
+// 4. Post- Decrement
+//console.log(a--); //10
+
+
+
+
+// Assignment Operator
+// --------------------
+
+// let:{ a=10 ,
+// b=20};
+
+// console.log(a+=11);
+// console.log(a-=3);
+
+
+// Comparison Operator
+// -------------------
+
+// <,>,<=,>=,==, !=,!==;
+
+// console.log(10<5);
+
+
+
+// Strict Equality(===) :- Data type and value are same then its give true
+// ---------------------
+// console.log('5'==5);
+
+// Loose Equality(==):- only one are same then its give true
+//-------------------
+// console.log('5'===5);
+
+
+
+
+
+// Ternary Operator
+// -----------------
+
+// let age=20;
+// let result=(age>18) ? 'i can vote' : 'i can not vote' ;
+// console.log(result);
+
+
+// Logical Operator
+//-------------------
+
+/* 1. AND(&&)
+   2. OR(||)
+   3. NOT(!)
+
+
+let ans=(false || false|| true)
+console.log(ans)
+
+let anss=(false && false&& true)
+console.log(anss)
+
+let an=(!false)
+console.log(an);
+*/
+
+
+
+/* Falsy And Truthy
+
+
+console.log(false||"Utkarsh");
+console.log(false && "Utkarsh");
+*/
+
+// Short Curciting
+// console.log(false || 7 || 22 || 33);
+
+
+
+/* Bitwise Operator
+   ---------------- */
+   console.log(5<<1);// Left shift
+
+   console.log(5>>1);
+   
+
