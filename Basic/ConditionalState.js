ba4e859f7d7a2fd271659957f1f3ab5ccda978ac
Utkarsh Misal <110691605+Utkarsh-876@users.noreply.github.com>
2024-08-02 12:51:18 +0000

    container config done

---
+++ Basic/ConditionalState.js
@@ -0,0 +1,36 @@
+/* If Statement
+   -------------
+let age=52;
+if(age>=18)
+{
+    console.log("Can vote");
+}
+else{
+    console.log("Can not vote");
+}
+*/
+
+
+
+/* If Else-If Statement
+-----------------------
+*/
+ let number=1;
+
+ if(number==1)
+ {
+    console.log('A');
+ }
+ else if(number==2)
+ {
+    console.log('B');
+ }
+ else if(number==3)
+ {
+    console.log('C');
+ }
+ else{
+    console.log("YEah number is correct");
+ }
+
+
