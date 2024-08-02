f23831363455070a90bfea346f824b8f659665ac
Utkarsh Misal <110691605+Utkarsh-876@users.noreply.github.com>
2024-08-02 04:55:24 +0000

    container config done

---
+++ Basic/dataType.js
@@ -0,0 +1,18 @@
+AccountId=213
+AccountName= "Utkarsh"
+AccountType= "Saving"
+AccountCity= "Nagpur"
+
+//Data Type
+
+// number => 123
+// String => ""
+// bigInt 
+// Boolean=> true/false
+// null => Object
+// undefine => undefine
+// Symbol => 
+
+console.log(typeof AccountName)
+console.log(typeof null)
+console.log(typeof undefined)
