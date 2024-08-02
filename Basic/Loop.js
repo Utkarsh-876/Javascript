2c2be0741d3dec7b0047a2da4ed345cddbbd5aa5
Utkarsh Misal <110691605+Utkarsh-876@users.noreply.github.com>
2024-08-02 15:56:24 +0000

    container config done

---
+++ Basic/Loop.js
@@ -0,0 +1,89 @@
+// To print aything for respected time
+
+// for(let i=0;i<=5;i++)
+// {
+//     console.log("My name is Utkarsh");
+// }
+
+
+
+//2. To print number from 1 to 10;
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(i)
+// }
+
+
+
+/*3. print above que in reverse manner
+
+for(let i=10;i>=1;i--)
+{
+    console.log(i);
+}*/
+
+
+/* 4.Print statement using break
+
+for(let i=1;i<=6;i++)
+{
+    if(i==4)
+        break;
+    else
+    console.log(i);
+}
+*/
+
+
+/*5. By using Continue
+ -----------------------
+
+ for(let i=1;i<=4;i++)
+ {
+    if(i==3)
+        continue;
+    else
+    console.log(i);
+ }
+*/
+
+ // While Loop 
+
+//  let i=1;  // Initialization
+//  while(i<=10)// condition
+//  {
+//     console.log(i);
+//     i++;// Updation
+//  }
+
+
+// Example
+/*
+let i=1;
+while(i<5)
+{
+    if(i==3)
+    {
+        i++;
+        continue;
+    }
+    else
+    {
+        console.log("hiii");
+    }
+    console.log(i);
+    i++;
+}
+*/
+
+
+
+
+/* Do While Loop
+-----------------
+*/
+let i=1;
+do{
+    console.log("Utkarsh");
+    i++
+}while(i<5);
