085b566444d3af118b52d886cb9a47da7df2b3a2
Utkarsh Misal <110691605+Utkarsh-876@users.noreply.github.com>
2024-08-04 12:53:39 +0000

    container config done

---
+++ Basic/String.js
@@ -0,0 +1,42 @@
+/* 1. String Literal(`` Backticks)
+----------------------------------
+
+// let opt="Utkarsh";
+// let opt1= " Misal";
+// console.log(opt+opt1);
+
+// OR
+
+let a=`utarsh
+misal
+this is my
+name`;
+console.log(a);
+
+
+2. toUpperCase and toLowerCase
+--------------------------------
+let a= "Utkarsh";
+let b="MISAL";
+console.log(a.toUpperCase());
+console.log(b.toLowerCase());
+*/
+
+
+
+/* 3. SubString Method
+------------------------
+
+let str="Utkarsh";
+console.log(str.substring(2,4));
+*/
+
+
+/*4. Split();
+*/
+let sentence="Hello jee kaise ho saare";
+let words=sentence.split(' ');
+console.log(words);
+
+let sentences="Hello\\ jee\\ kaise\\ ho\\ saare";
+let slash=sentences.split('\\');
